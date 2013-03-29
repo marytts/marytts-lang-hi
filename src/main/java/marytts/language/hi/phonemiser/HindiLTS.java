76cce9e978f9a62e461199a80304cbc5dca32497
Sathish Chandra Pammi <sathishc.pammi@gmail.com>
2013-03-29 14:32:53 +0100

    Hindi LTS updates

--- src/main/java/marytts/language/hi/phonemiser/HindiLTS.java
+++ src/main/java/marytts/language/hi/phonemiser/HindiLTS.java
@@ -20,7 +20,9 @@
 package marytts.language.hi.phonemiser;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -35,6 +37,10 @@ public class HindiLTS {
     private HashMap<String, String> UTF8toPhoneTypes;
     private ArrayList<String> listPhoneSym;
     private ArrayList<String> listPhoneTypes;
+    private ArrayList<String> listConTypes;
+    
+    
+    private ArrayList<String> utf8CharList;
     
     /**
      * TeluguLTS constructor
@@ -44,18 +50,14 @@ public class HindiLTS {
     public HindiLTS(InputStream utf8toit3mapStream) throws IOException{
         this.loadPhoneSymbolsAndTypes(utf8toit3mapStream);
     }
+  
     
-    /**
-     * Get it3 pronunciation for a word
-     * @param word
-     * @return
-     * @throws IOException
-     */
     public String phonemise(String word) throws IOException
     {
-        ArrayList<String> utf8CharList = readUTF8String(word);
+        utf8CharList = readUTF8String(word);
         listPhoneSym = new ArrayList<String>();
         listPhoneTypes = new ArrayList<String>();
+        listConTypes = new ArrayList<String>();
         
         Iterator<String> listrun = utf8CharList.iterator();
         while(listrun.hasNext())
@@ -67,11 +69,22 @@ public class HindiLTS {
             if(phoneType == null) phoneType = "#";
             listPhoneSym.add(phoneSymbol);
             listPhoneTypes.add(phoneType);
-            //System.out.println(utf8Char+" "+phoneSymbol+" "+phoneType);
+            if ("CON".endsWith(phoneType)) {
+            	listConTypes.add("U");
+            	System.out.println(utf8Char+" "+phoneSymbol+" "+phoneType+ " U");
+            } else {
+            	listConTypes.add("#");
+            	System.out.println(utf8Char+" "+phoneSymbol+" "+phoneType+" #");
+            }
+            
         }
         
-        removeUnknownSymbols();
+        removeUnknownSymbols(); 
+        //printArrayList(listPhoneSym);
+        //printArrayList(listPhoneTypes);
+        //printArrayList(listConTypes);
         schwaHandler();
+        //printArrayList(listPhoneSym);
         removeHal();
         syllabify();
         putStressMark();
@@ -199,6 +212,103 @@ public class HindiLTS {
     }
         
    
+    
+    /**
+     * verification for 'KA' varg based on unicode chart
+     * @param uniCodeHex
+     * @return
+     */
+    private boolean isBelongs2KAVarga(String uniCodeHex) {
+        int unicode = hexString2Int(uniCodeHex);
+        int minFVChart = hexString2Int("0915"); 
+        int maxFVChart = hexString2Int("0919");
+        
+        if ( unicode >= minFVChart && unicode <= maxFVChart ) {
+            return true;
+        }
+        return false;
+    }
+    
+    /**
+     * verification for 'CHA' varg based on unicode chart
+     * @param uniCodeHex
+     * @return
+     */
+    private boolean isBelongs2CHAVarga(String uniCodeHex) {
+        int unicode = hexString2Int(uniCodeHex);
+        int minFVChart = hexString2Int("091A"); 
+        int maxFVChart = hexString2Int("091E");
+        
+        if ( unicode >= minFVChart && unicode <= maxFVChart ) {
+            return true;
+        }
+        return false;
+    }
+    
+    /**
+     * verification for 'TA' varg based on unicode chart
+     * @param uniCodeHex
+     * @return
+     */
+    private boolean isBelongs2TAVarga(String uniCodeHex) {
+        int unicode = hexString2Int(uniCodeHex);
+        int minFVChart = hexString2Int("091F"); 
+        int maxFVChart = hexString2Int("0923");
+        
+        if ( unicode >= minFVChart && unicode <= maxFVChart ) {
+            return true;
+        }
+        return false;
+    }
+    
+    /**
+     * verification for 'THA' varg based on unicode chart
+     * @param uniCodeHex
+     * @return
+     */
+    private boolean isBelongs2THAVarga(String uniCodeHex) {
+        int unicode = hexString2Int(uniCodeHex);
+        int minFVChart = hexString2Int("0924"); 
+        int maxFVChart = hexString2Int("0929");
+        
+        if ( unicode >= minFVChart && unicode <= maxFVChart ) {
+            return true;
+        }
+        return false;
+    }
+    
+    /**
+     * verification for 'PA' varg based on unicode chart
+     * @param uniCodeHex
+     * @return
+     */
+    private boolean isBelongs2PAVarga(String uniCodeHex) {
+        int unicode = hexString2Int(uniCodeHex);
+        int minFVChart = hexString2Int("092A"); 
+        int maxFVChart = hexString2Int("092E");
+        
+        if ( unicode >= minFVChart && unicode <= maxFVChart ) {
+            return true;
+        }
+        return false;
+    }
+    
+    /**
+     * verification for 'YA' varg based on unicode chart
+     * @param uniCodeHex
+     * @return
+     */
+    private boolean isBelongs2YAVarga(String uniCodeHex) {
+        int unicode = hexString2Int(uniCodeHex);
+        int minFVChart = hexString2Int("092F"); 
+        int maxFVChart = hexString2Int("0939");
+        
+        if ( unicode >= minFVChart && unicode <= maxFVChart ) {
+            return true;
+        }
+        return false;
+    }
+    
     public ArrayList<String> readUTF8String(String word) throws IOException
     {
         CharBuffer cbuf = CharBuffer.wrap(word);
@@ -238,33 +348,31 @@ public class HindiLTS {
     }
     
     
+    
     public void makeProperIt3(String filename) throws IOException
     {
         
-        ArrayList<String> utf8CharList = readUTF8File(filename);
-        ArrayList<String> lPhoneSym = new ArrayList<String>();
-        ArrayList<String> lPhoneTypes = new ArrayList<String>();
-        
-        Iterator<String> listrun = utf8CharList.iterator();
-        while(listrun.hasNext())
-        {
-            String utf8Char    = listrun.next();
-            String phoneSymbol = UTF8toPhoneSymbols.get(utf8Char);
-            String phoneType   = UTF8toPhoneTypes.get(utf8Char);
-            if(phoneSymbol == null) phoneSymbol = getAsciiChar(utf8Char);
-            if(phoneType == null) phoneType = "#";
-            lPhoneSym.add(phoneSymbol);
-            lPhoneTypes.add(phoneType);
-            System.out.println(utf8Char+" "+phoneSymbol+" "+phoneType);
-        }
-        
-        printArrayList(lPhoneSym);
-        printArrayList(lPhoneTypes);
-        
-        lPhoneSym = schwaHandler(lPhoneSym,lPhoneTypes);
-        lPhoneSym = removeHal(lPhoneSym,lPhoneTypes);
-        printArrayList(lPhoneSym);
-        
+    	utf8CharList = readUTF8File(filename);
+        listPhoneSym = new ArrayList<String>();
+        listPhoneTypes = new ArrayList<String>();
+         
+         Iterator<String> listrun = utf8CharList.iterator();
+         while(listrun.hasNext())
+         {
+             String utf8Char    = listrun.next();
+             String phoneSymbol = UTF8toPhoneSymbols.get(utf8Char);
+             String phoneType   = UTF8toPhoneTypes.get(utf8Char);
+             if(phoneSymbol == null) phoneSymbol = getAsciiChar(utf8Char);
+             if(phoneType == null) phoneType = "#";
+             listPhoneSym.add(phoneSymbol);
+             listPhoneTypes.add(phoneType);
+             System.out.println(utf8Char+" "+phoneSymbol+" "+phoneType);
+         }
+         
+         printArrayList(listPhoneSym);
+         printArrayList(listPhoneTypes);
+         schwaHandler();
+         printArrayList(listPhoneSym);
     }
     
  
@@ -328,12 +436,12 @@ public class HindiLTS {
     }
 
 
-    /**
+/*    *//**
      * Schwa handler for telugu
      * @param lPhoneSym
      * @param lPhoneTypes
      * @return
-     */
+     *//*
     private ArrayList<String> schwaHandler(ArrayList<String> lPhoneSym,
             ArrayList<String> lPhoneTypes) {
         
@@ -356,9 +464,9 @@ public class HindiLTS {
         return lPhoneSym;
     }
 
-    /**
+    *//**
      * Schwa handler for telugu
-     */
+     *//*
     private void schwaHandler() {
         
         String prev, next;
@@ -382,8 +490,83 @@ public class HindiLTS {
             }
             
         }
-    }
+    }*/
 
+    private void schwaHandlerNew() {
+    	
+    	
+    	
+    }
+    
+    /**
+     * Schwa handler 
+     */
+    private void schwaHandler() {
+        
+        String prevType, nextType;
+        String prevPhone, nextPhone;
+        String prevUchar, nextUchar;
+        boolean isFinalCharacter = false;
+        
+        for(int i=0; i<listPhoneTypes.size(); i++){
+            
+            //if(listPhoneTypes.get(i) == null) continue;
+            //if(listPhoneTypes.get(i+1) == null) ;
+            prevType  = listPhoneTypes.get(i);
+            prevPhone = this.listPhoneSym.get(i);
+            prevUchar = this.utf8CharList.get(i);
+            
+            if ( (i+1) < listPhoneTypes.size() ) {
+                nextType = listPhoneTypes.get(i+1);
+                nextPhone = this.listPhoneSym.get(i+1);
+                nextUchar = this.utf8CharList.get(i+1);
+            } else {
+                nextType = listPhoneTypes.get(i);
+                nextPhone = this.listPhoneSym.get(i);
+                nextUchar = this.utf8CharList.get(i);
+                isFinalCharacter = true;
+            }
+            
+            // Bindu handling
+            if (prevUchar.equals("0902")) {
+                if (isFinalCharacter == true) {
+                    listPhoneTypes.set(i, "CON");
+                    listPhoneSym.set(i, "ng~");
+                    utf8CharList.set(i, "0919");
+                } else if ( this.isBelongs2TAVarga(nextUchar) ) {
+                    listPhoneTypes.set(i, "CON");
+                    listPhoneSym.set(i, "n");
+                    utf8CharList.set(i, "0928");
+                } else if ( this.isBelongs2PAVarga(nextUchar) ) {
+                    listPhoneTypes.set(i, "CON");
+                    listPhoneSym.set(i, "m");
+                    utf8CharList.set(i, "092E");
+                } else if ( this.isBelongs2KAVarga(nextUchar) ) {
+                    listPhoneTypes.set(i, "CON");
+                    listPhoneSym.set(i, "ng~");
+                    utf8CharList.set(i, "0919");
+                }
+            }
+            
+            if (isFinalCharacter == true) {
+                break;
+            }
+            
+            //System.err.println(prevType+" "+prevPhone+" "+prevUchar+" "+nextType+" "+nextPhone+" "+nextUchar);
+                
+            if ( (prevType.equals("CON") && nextType.equals("CON")) || (prevType.equals("CON") && nextType.equals("SYM")) ) {
+                listPhoneTypes.add(i+1, "VOW");
+                listPhoneSym.add(i+1, "a");
+                this.utf8CharList.add(i+1,"0905");
+            } else if ( prevType.equals("CON") && isFullVowel(nextUchar) ) {
+                listPhoneTypes.add(i+1, "VOW");
+                listPhoneSym.add(i+1, "a");
+                this.utf8CharList.add(i+1,"0905");
+            }
+            
+        }
+    }
+    
     /**
      * print array list 
      * @param aList
@@ -394,9 +577,51 @@ public class HindiLTS {
         while(listrun.hasNext())
         {
             //System.out.print(listrun.next()+" ");
-            System.out.print(listrun.next());
+            System.out.print(" " + listrun.next());
         }
         System.out.println();
+    }
+    
+
+    /**
+     * verification for full vowel range based on unicode chart
+     * @param uniCodeHex
+     * @return
+     */
+    private boolean isFullVowel(String uniCodeHex) {
+        int unicode    = hexString2Int(uniCodeHex);
+        int minFVChart = hexString2Int("0904"); 
+        int maxFVChart = hexString2Int("0914");
+        
+        if ( unicode >= minFVChart && unicode <= maxFVChart ) {
+            return true;
+        }
+        return false;
+    }
+    
+    /**
+     * Hex-decimal representation for a given string
+     * @param ch
+     * @return
+     */
+    private String int2HexString(int ch) 
+    {
+        String hex = Integer.toHexString(ch).toUpperCase();
+        switch (hex.length()) {
+            case 3  : return "0" + hex;
+            case 2  : return "00" + hex;
+            case 1  : return "000" + hex;
+            default : return hex;
+        }
+    }
+    
+    /**
+     * converting hexcode to integer value
+     * @param hexCode
+     * @return
+     */
+    private int hexString2Int (String hexCode) {
+        return Integer.parseInt(hexCode, 16);  
     }
     
     /**
@@ -405,8 +630,8 @@ public class HindiLTS {
      */
     public static void main(String[] args) throws IOException {
         
-        HindiLTS utf8r = new  HindiLTS(new FileInputStream("~/openmary/lib/modules/te/lexicon/UTF8phone.te.list"));
-        
+        //HindiLTS utf8r = new  HindiLTS(new FileInputStream("~/openmary/lib/modules/te/lexicon/UTF8phone.te.list"));
+        HindiLTS utf8r = new  HindiLTS(new FileInputStream("/Users/sathish/Work/BitBucket/marytts/marytts-lang-hi/src/main/resources/marytts/language/hi/lexicon/UTF8toIT3.hi.list"));
         //utf8r.makeProperIt3("/home/sathish/Desktop/telugu-utf8-txt.done.data");
         //String nameString = "\u0C05\u0C38\u0C1F\u0C08\u0C05\u0C37";
         //PrintWriter pw = new PrintWriter(new OutputStreamWriter(new FileOutputStream("telugu-utf.txt"), "UTF8"));
@@ -414,7 +639,7 @@ public class HindiLTS {
         //pw.flush();
         //pw.close();
         
-        System.out.println("Result : "+utf8r.phonemise("ప్రకారం"));
+        System.out.println("Result : "+utf8r.phonemise("आपका"));
     }
 
 }
